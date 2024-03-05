f9e226e158d76eb2ebbee587fcee81cf563ea866
Inam <trueinam@yahoo.com>
2024-03-05 20:28:33 +0100

    basic functionality added for downloading html and images

---
+++ WebScrapper/FileDownloader.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+
+namespace WebScrapper
+{
+    public class FileDownloader
+    {
+        private List<string> DownloadedFiles = new();
+        public string BaseUrl { get; set; }
+        public FileDownloader(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+
+        public async Task Download(string filePath, bool isHomePage = false)
+        {
+            if (DownloadedFiles.Contains(filePath) || string.IsNullOrEmpty(filePath))
+            {
+                return;
+            }
+
+            DownloadedFiles.Add(filePath);
+
+            filePath = CleanUrl(filePath);
+
+            using var httpClient = new HttpClient();
+            var resp = await httpClient.GetAsync(BaseUrl + filePath);
+
+
+            var data = !resp.IsSuccessStatusCode && resp.StatusCode != HttpStatusCode.NotFound ? throw new Exception("Something went wrong!")
+                 : await resp.Content.ReadAsByteArrayAsync();
+
+            if (isHomePage || filePath.LastIndexOf('/') == -1)
+            {
+                await SaveFile(filePath, data);
+                return;
+            }
+
+            var directoryName = filePath.Substring(0, filePath.LastIndexOf('/'));
+
+            if (!Directory.Exists(directoryName) && !string.IsNullOrEmpty(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+
+            await SaveFile(filePath, data);
+
+        }
+
+        private static async Task SaveFile(string filePath, byte[] data)
+        {
+            using var fs = File.Create(filePath);
+            await fs.WriteAsync(data, 0, data.Length);
+            fs.Close();
+        }
+
+        private string CleanUrl(string url)
+        {
+            url = url.StartsWith(BaseUrl) ? url.Replace(BaseUrl, string.Empty) : url;
+
+            url = url.StartsWith("../") ? url.Replace("../", string.Empty) : url;
+
+            return url;
+        }
+    }
+}
--- WebScrapper/Program.cs
+++ WebScrapper/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace WebScrapper
+﻿using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
+
+namespace WebScrapper
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static string homePageUrl = "https://books.toscrape.com/";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Execution Started!");
+
+            await DownloadWebSite();
+
+            Console.WriteLine("Execution Completed!");
+        }
+
+        private static async Task DownloadWebSite()
+        {
+            var web = new HtmlWeb();
+            var pageIndex = 0;
+            HtmlDocument document;
+            FileDownloader fileDownloader = new FileDownloader(homePageUrl);
+
+            while (pageIndex < 50)
+            {
+                /// First download the html page
+                /// 
+                if (pageIndex == 0)
+                {
+                    await fileDownloader.Download("index.html", true);
+                }
+                else
+                {
+                    await fileDownloader.Download($"{homePageUrl}catalogue/page-{pageIndex}.html", false);
+                }
+
+                var currenPageUrl = pageIndex == 0 ? homePageUrl : $"{homePageUrl}catalogue/page-{pageIndex}.html";
+
+                document = web.Load(currenPageUrl);
+
+                var productHTMLElements = document.DocumentNode.QuerySelectorAll("li");
+
+
+                foreach (var productHTMLElement in productHTMLElements)
+                {
+                    var url = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("a")?.Attributes["href"].Value);
+                    var image = HtmlEntity.DeEntitize(productHTMLElement.QuerySelector("img")?.Attributes["src"].Value);
+
+                    if (image != null)
+                    {
+                        await fileDownloader.Download(image);
+                    }
+                    if (url != null)
+                    {
+                        await fileDownloader.Download(url);
+                    }
+                }
+                pageIndex++;
+            }
+
         }
     }
 }
